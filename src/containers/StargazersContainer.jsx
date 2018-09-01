1b77e182c74799fbc93c08f6d32403d26ab7159d
Raphael Thomazella <thomazella9@gmail.com>
2018-09-01 10:29:36 -0300

    worked on core methods

--- src/containers/StargazersContainer.jsx
+++ src/containers/StargazersContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container } from "reakit";
 import "whatwg-fetch";
+import { flatten } from "lodash";
 
 const initialState = {
   stars: 0,
@@ -10,17 +11,51 @@ const initialState = {
 const USER = "diegohaz";
 const DEPTH = 25;
 
-const countStars = repos =>
-  repos.reduce((acc, it) => it.stargazers_count + acc, 0);
+const githubData = async query => {
+  let data;
+  let err;
 
-const findPairs = repos =>
-  repos.reduce((acc, it) => it.stargazers_count + acc, 0);
+  try {
+    const result = await fetch(`https://api.github.com/${query}`);
+    const json = await result.json();
+    data = json;
+  } catch (error) {
+    err = error;
+  }
+
+  return [data, err];
+};
+
+const reposFrom = user => `users/${user}/repos`;
+const issuesIn = repoFullName => `repos/${repoFullName}/issues?state=all`;
+
+const countStars = async user => {
+  const [repos] = await githubData(reposFrom(user));
+  return repos.reduce((acc, it) => it.stargazers_count + acc, 0);
+};
+
+const assertStars = async (user, goal) => goal === (await countStars(user));
+
+const findPossiblePairs = async (user, depth) => {
+  const [repos] = await githubData(reposFrom(user));
+  const promises = repos
+    .slice(0, depth)
+    .map(repo => githubData(issuesIn(repo.full_name)));
+  const issuesPerRepo = await Promise.all(promises);
+  const issues = issuesPerRepo.reduce((acc, it) => {
+    const [data] = it;
+    return [...acc, ...data];
+  }, []);
+
+  return issues.map(issue => issue && issue.user && issue.user.login);
+};
 
 const onMount = async ({ setState }) => {
   try {
-    const result = await fetch(`https://api.github.com/users/${USER}/repos`);
-    const repos = await result.json();
-    setState({ stars: countStars(repos) });
+    // const stars = countStars(USER);
+    const foo = await findPossiblePairs(USER, 3);
+
+    // setState({ stars });
   } catch (e) {
     setState({ error: e });
   }
